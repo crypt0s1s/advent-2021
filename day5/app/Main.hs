c4d922066069349a09c82795e739adb0f046672f
Josh <j4sumskas@gmail.com>
2021-12-06 00:05:37 +1100

    Added distinct p1 / p2 parts for day 5

--- day5/app/Main.hs
+++ day5/app/Main.hs
@@ -7,7 +7,7 @@ import qualified Data.Map as Map
 main :: IO ()
 main = do
     inp <- lines <$> readFile "day5.txt"
-    print $ greaterThan2 $ createMap $ concatMap parseInput' inp
+    print $ greaterThan2 $ createMap $ concat $ filter p2 (map parseInput inp)
 
 newtype Point = Point (Int, Int)
   deriving Show
@@ -19,13 +19,18 @@ instance Ord Point where
     (Point (x1,y1)) <= (Point (x2,y2)) | x1 == x2 = y1 < y2
                                        | otherwise = x1 < x2
 
+p1 :: [Point] -> Bool
+p1 ((Point (x1,y1)):(Point (x2,y2)):_) = x1 == x2 || y1 == y2
+p1 _ = True
+
+p2 :: [Point] -> Bool
+p2 _ = True
+
 parseInput :: String -> [Point]
-parseInput ls | isStraight = genPoints x1 x2 y1 y2
-              | otherwise = []
+parseInput ls = genPoints x1 x2 y1 y2
                 where w = words ls
                       [x1,y1] = map read (splitOn "," (head w))
                       [x2,y2] = map read (splitOn "," (last w))
-                      isStraight = (x1 == x2) || (y1 == y2)
 
 genPoints :: Int -> Int -> Int -> Int -> [Point]
 genPoints x1 x2 y1 y2 | abs (x1 - x2) > abs (y1 - y2) = zipWith makePoint xs (cycle  ys)
@@ -47,10 +52,3 @@ insertPointToMap m point = Map.insertWith (+) point 1 m
 
 greaterThan2 :: Map.Map Point Int -> Int
 greaterThan2 = foldr (\x -> if x > 1 then (1+) else (0+)) 0
-
-
-parseInput' :: String -> [Point]
-parseInput' ls = genPoints x1 x2 y1 y2
-                 where w = words ls
-                       [x1,y1] = map read (splitOn "," (head w))
-                       [x2,y2] = map read (splitOn "," (last w))
